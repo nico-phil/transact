cc0ae0ee60df2447541d59bf8692643765c0c26e
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2025-06-01 20:11:53 +0300

    add create wallet handler

---
+++ utils/json.go
@@ -0,0 +1,27 @@
+package utils
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func ReadJSON(r *http.Request, dst any) error{
+	err := json.NewDecoder(r.Body).Decode(&dst)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func WriteJSON(w http.ResponseWriter, statusCode int,  data any) error {
+	byteData, err := json.MarshalIndent(data, "", "\t")
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	w.Write(byteData)
+
+	return  nil
+}
