fef454cdd3739028db2321dc95ea27a8f99f106a
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2025-06-01 20:28:34 +0300

    setup blockchain server

---
+++ blockchain_server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/nico-phil/transact/config"
+)
+
+func main(){
+	blockchainServer := NewBlockchainServer(config.GetServerPort())
+	err := blockchainServer.Run()
+	if err != nil {
+		log.Fatal("cannot start wallet server")
+	}
+}
