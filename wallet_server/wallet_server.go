8e0ba77bf0f0b3d94e8f60b386a7149fa9e80141
Nicolas Philibert <phil@Nicolass-MacBook-Pro.local>
2025-06-04 22:13:05 +0300

    add get balance api for wallet

--- wallet_server/wallet_server.go
+++ wallet_server/wallet_server.go
@@ -98,11 +98,46 @@ func (ws *WalletServer) CreateTransactions(w http.ResponseWriter, r *http.Reques
 	utils.WriteJSON(w, http.StatusCreated, wrapper{"message": "sucess"})
 }
 
+func(ws *WalletServer) GetBalance(w http.ResponseWriter, r *http.Request){
+	url := ""
+	req, err:= http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusBadRequest, wrapper{"error": "cannot create new req"})
+		return
+	}
+	
+	amountResponse, err := ws.Client.Do(req)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusNotFound, wrapper{"error": "reqeut failed"})
+		return
+	}
+	
+	if amountResponse.StatusCode != http.StatusOK {
+		utils.WriteJSON(w, http.StatusNotFound, wrapper{"error": "request failed with bad code status"})
+		return
+	}
+
+	defer amountResponse.Body.Close()
+	
+	type AmountResponse struct {
+		Amount float64 `json:"amount"`
+	}
+
+	var aR AmountResponse
+	json.NewDecoder(amountResponse.Body).Decode(aR)
+	if err != nil {
+		utils.WriteJSON(w, http.StatusBadRequest, wrapper{"error": "fail to read body"})
+	}
+	
+	utils.WriteJSON(w, http.StatusCreated, wrapper{"amount": aR.Amount})
+}
+
 func (ws *WalletServer) Run() error {
 	router := http.NewServeMux()
 
 	router.HandleFunc("/", ws.Index)
 	router.HandleFunc("/wallet", ws.CreateWalletHandler)
 	router.HandleFunc("POST /wallet/transactions", ws.CreateTransactions)
+	router.HandleFunc("POST /wallet/balance", ws.GetBalance)
 	return http.ListenAndServe(fmt.Sprintf(":%d", ws.Port), router)
 }
